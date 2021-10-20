03a2ece9b502c375460ad69cb2714b34caaabef8
Dmitry Kilin <coding@kilin.biz>
2021-10-20 22:56:47 +0300

    fix: remove unneccessary  videoplayer component

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,17 +1,62 @@
 import {Link} from 'react-router-dom';
 import {Film} from '../../types/film';
-import VideoPlayer from '../videoplayer/videoplayer';
+// import VideoPlayer from '../videoplayer/videoplayer';
+import {useEffect, useRef, useState} from 'react';
 
 type SmallFilmcardProps = {
   film: Film,
 }
 
+
 function SmallFilmCard({film}: SmallFilmcardProps): JSX.Element {
   const filmPath = `/films/${film.id}`;
 
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleMouseEnter = () => {
+    setIsPlaying(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsPlaying(false);
+  };
+
+  const stopPlay = (video: HTMLVideoElement | null): void => {
+    if (video) {
+      video.load();
+    }
+  };
+
+  useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>;
+    if (isPlaying) {
+      timeout = setTimeout(() => videoRef.current?.play(), 1000);
+    } else {
+      stopPlay(videoRef.current);
+    }
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [isPlaying]);
+
   return (
-    <article className="small-film-card catalog__films-card">
-      <VideoPlayer film={film}/>
+    <article className="small-film-card catalog__films-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="small-film-card__image">
+        <video
+          poster={film.posterImage}
+          src={film.previewVideoLink}
+          ref={videoRef}
+          preload="none"
+          loop
+          muted
+        />
+      </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={filmPath}>{film.name}</Link>
       </h3>
