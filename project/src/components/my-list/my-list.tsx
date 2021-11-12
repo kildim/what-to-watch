e2b926c2adfc154665bbd108f3239b667f89f7d4
Dmitry Kilin <coding@kilin.biz>
2021-11-12 19:33:34 +0300

    fix:  remove unnecessary comments

--- project/src/components/my-list/my-list.tsx
+++ project/src/components/my-list/my-list.tsx
@@ -26,21 +26,6 @@ function MyList(props: PropsFromRedux): JSX.Element {
 
         <h1 className="page-title user-page__title">My list</h1>
 
-        {/*<ul className="user-block">*/}
-        {/*  <li className="user-block__item">*/}
-        {/*    <div className="user-block__avatar">*/}
-        {/*      <img*/}
-        {/*        src="img/avatar.jpg"*/}
-        {/*        alt="User avatar"*/}
-        {/*        width="63"*/}
-        {/*        height="63"*/}
-        {/*      />*/}
-        {/*    </div>*/}
-        {/*  </li>*/}
-        {/*  <li className="user-block__item">*/}
-        {/*    <a className="user-block__link">Sign out</a>*/}
-        {/*  </li>*/}
-        {/*</ul>*/}
         <UserBlock />
       </header>
 
