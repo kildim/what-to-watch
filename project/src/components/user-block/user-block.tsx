018e6f33e2342f3e5565aba724afa34056f8aa4d
Dmitry Kilin <coding@kilin.biz>
2021-11-13 14:41:58 +0300

    fit:  add Avatar

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,22 +1,45 @@
-function UserBlock(): JSX.Element {
-  const genSignLinkListItem = () => (
-    <li className="user-block__item">
-      <a className="user-block__link">Sign out</a>
-    </li>
-  );
-  const genAvatarListItem = () => (
-    <li className="user-block__item">
-      <div className="user-block__avatar">
-        <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-      </div>
-    </li>
-  );
+import {connect, ConnectedProps} from 'react-redux';
+import {StateType} from '../../types/state';
+import {AuthorizationStatus} from '../../const';
+
+const mapStateToProps = ({authorizationStatus, userInfo}: StateType) => ({
+  authorizationStatus, userInfo,
+});
+
+const connector = connect(mapStateToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+function UserBlock(props: PropsFromRedux): JSX.Element {
+  const {authorizationStatus, userInfo} = props;
+
   return (
     <ul className="user-block">
-      {genAvatarListItem()}
-      {genSignLinkListItem()}
+      {authorizationStatus === AuthorizationStatus.Auth && (
+        <>
+          <li className="user-block__item">
+            <div className="user-block__avatar">
+              <img
+                src={userInfo.avatarUrl}
+                alt="User avatar"
+                width="63"
+                height="63"
+              />
+            </div>
+          </li>
+          <li className="user-block__item">
+            <a className="user-block__link">Sign out</a>
+          </li>
+        </>
+      )}
+      {authorizationStatus !== AuthorizationStatus.Auth && (
+        <li className="user-block__item">
+          <a className="user-block__link">Sign in</a>
+        </li>
+      )}
     </ul>
   );
 }
 
-export default UserBlock;
+export {UserBlock};
+export default connector(UserBlock);
